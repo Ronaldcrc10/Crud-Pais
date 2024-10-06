203c4c2431baa2a5b9574b064ba88853be9c1c1b
Ronaldo <ronaldcollo172@gmail.com>
2024-10-06 14:08:52 -0500

    Nueva version Listado de Paises :D

--- app/Http/Controllers/PaisController.php
+++ app/Http/Controllers/PaisController.php
@@ -15,7 +15,13 @@ class PaisController extends Controller
      */
     public function index()
     {
-        $paises = Pais::all();
+        //$paises = Pais::all();
+
+        $paises = DB::table('tb_pais')
+        ->join('tb_departamento', 'tb_pais.pais_codi', '=', 'tb_departamento.pais_codi')
+        ->select('tb_pais.*', 'tb_departamento.depa_nomb')
+        ->get();
+
         return view('pais.index', ['paises' => $paises]);
     }
 
